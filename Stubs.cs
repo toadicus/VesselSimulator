9ee48f645d297279a48471852dd5543c204bb598
Andy Wilkinson <andy@wtribe.com>
2015-04-27 20:40:12 -0700

    Added Stubs.cs, which provides a few stubs for Engineer methods that are closely coupled with VesselSimulator.

---
+++ Stubs.cs
@@ -0,0 +1,34 @@
+﻿// VesselSimulator © 2015 toadicus
+//
+// This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a
+// copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+using System;
+
+namespace KerbalEngineer.Helpers
+{
+    public static class CelestialBodies
+    {
+        public static CelestialBody SelectedBody
+        {
+            get;
+            set;
+        }
+
+        public static double GetDensity(this CelestialBody body, double altitude)
+        {
+            return body.GetDensity(body.GetPressure(altitude), body.GetTemperature(altitude));
+        }
+    }
+}
+
+namespace KerbalEngineer.Editor
+{
+    public static class BuildAdvanced
+    {
+        public static float Altitude
+        {
+            get;
+            set;
+        }
+    }
+}
